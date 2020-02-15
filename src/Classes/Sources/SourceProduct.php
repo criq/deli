c8ee44bce472b8727f257e3b39576c8051b5e93d
Jan Jablunka <jablunka@gmail.com>
2020-02-15 19:58:40 +0100

    incremental

--- src/Classes/Sources/SourceProduct.php
+++ src/Classes/Sources/SourceProduct.php
@@ -2,40 +2,43 @@
 
 namespace Deli\Classes\Sources;
 
-abstract class SourceProduct {
-
+abstract class SourceProduct
+{
 	public $product;
 
-	public function __construct($product) {
+	public function __construct($product)
+	{
 		$this->product = $product;
 	}
 
-	public function getProduct() {
+	public function getProduct()
+	{
 		return $this->product;
 	}
 
-	public function getSource() {
+	public function getSource()
+	{
 		return $this->getProduct()->getSource();
 	}
 
-	public function getURL() {
+	public function getURL()
+	{
 		return $this->getProduct()->uri;
 	}
 
-	public function getSrc() {
-		return \Katu\Cache::get([__CLASS__, __FUNCTION__, __LINE__], $this->product->getSource()::CACHE_TIMEOUT, function($url) {
-
+	public function getSrc()
+	{
+		return \Katu\Cache::get([__CLASS__, __FUNCTION__, __LINE__], $this->product->getSource()::CACHE_TIMEOUT, function ($url) {
 			$curl = new \Curl\Curl;
 			$curl->setOpt(CURLOPT_TIMEOUT, 30);
 			$curl->setOpt(CURLOPT_CONNECTTIMEOUT, 30);
 
 			return $curl->get($url);
-
 		}, $this->getURL());
 	}
 
-	public function getDOM() {
+	public function getDOM()
+	{
 		return \Katu\Utils\DOM::crawlHtml($this->getSrc());
 	}
-
 }
--- src/Classes/Sources/countrylife_cz/SourceProduct.php
+++ src/Classes/Sources/countrylife_cz/SourceProduct.php
@@ -2,18 +2,15 @@
 
 namespace Deli\Classes\Sources\countrylife_cz;
 
-class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
-
-	public function getURL() {
-		return 'https://www.countrylife.cz' . $this->getProduct()->uri;
-	}
-
+class SourceProduct extends \Deli\Classes\Sources\SourceProduct
+{
 	/****************************************************************************
 	 * Product information.
 	 */
-	public function loadProductInfos() {
+	public function loadProductInfos()
+	{
 		$productInfos = new \Deli\Classes\ProductInfos;
-
+		
 		foreach (array_filter(array_map('trim', explode('<h3>', $this->getDOM()->filter('#content')->html()))) as $line) {
 			try {
 				list($title, $text) = explode('</h3>', $line);
@@ -29,7 +26,8 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Allergens.
 	 */
-	public function loadAllergens() {
+	public function loadAllergens()
+	{
 		return \Deli\Models\ProductAllergen::getCodesFromStrings([
 			$this->loadProductInfos()->filterByTitle('Složení')[0]->text ?? null,
 			$this->loadProductInfos()->filterByTitle('Alergeny')[0]->text ?? null,
@@ -39,23 +37,22 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Product amount with unit.
 	 */
-	public function loadProductAmountWithUnit() {
+	public function loadProductAmountWithUnit()
+	{
 		return \Deli\Classes\AmountWithUnit::createFromString($this->getDOM()->filter('#popis-slozeni .ca-box h3')->text());
 	}
 
 	/****************************************************************************
 	 * Nutrients.
 	 */
-	public function loadNutrients() {
-		return $this->getDOM()->filter('#popis-slozeni .ca-box .table-content')->filter('tr')->each(function($e) {
-
+	public function loadNutrients()
+	{
+		return $this->getDOM()->filter('#popis-slozeni .ca-box .table-content')->filter('tr')->each(function ($e) {
 			if (preg_match('/^Energetická hodnota\s*([0-9\s\,\.]+)\s*kJ\s*\/\s*([0-9\s\,\.]+)\s*kcal$/u', trim($e->text()), $match)) {
 				return new \Deli\Classes\NutrientAmountWithUnit('energy', new \Deli\Classes\AmountWithUnit($match[1], 'kJ'));
 			} else {
 				return \Deli\Classes\NutrientAmountWithUnit::createFromStrings($e->filter('th')->text(), $e->filter('td')->text());
 			}
-
 		});
 	}
-
 }
