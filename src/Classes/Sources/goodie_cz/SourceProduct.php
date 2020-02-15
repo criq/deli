cd461b65d1a3bf01a01ceb66f681782723bb26bc
Jan Jablunka <jablunka@gmail.com>
2020-02-15 20:00:09 +0100

    incremental

--- src/Classes/Sources/goodie_cz/SourceProduct.php
+++ src/Classes/Sources/goodie_cz/SourceProduct.php
@@ -2,12 +2,13 @@
 
 namespace Deli\Classes\Sources\goodie_cz;
 
-class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
-
+class SourceProduct extends \Deli\Classes\Sources\SourceProduct
+{
 	/****************************************************************************
 	 * Allergens.
 	 */
-	public function loadAllergens() {
+	public function loadAllergens()
+	{
 		return \Deli\Models\ProductAllergen::getCodesFromStrings([
 			$this->getDOM()->filter('.basic-description')->text(),
 		]);
@@ -16,21 +17,22 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Product amount with unit.
 	 */
-	public function loadProductAmountWithUnit() {
+	public function loadProductAmountWithUnit()
+	{
 		return \Deli\Classes\AmountWithUnit::createFromString($this->getProduct()->getName());
 	}
 
 	/****************************************************************************
 	 * Allergens.
 	 */
-	public function loadNutrients() {
+	public function loadNutrients()
+	{
 		if (preg_match_all('/(?<nutrientName>Energie|Tuky|z toho nas\. mast\. kys\.|Sacharidy|z toho cukry|Bílkoviny|Sůl): (?<amountWithUnit>([0-9]+) (kJ|g))/', $this->getDOM()->filter('.basic-description')->text(), $matches, \PREG_SET_ORDER)) {
-			return array_map(function($match) {
+			return array_map(function ($match) {
 				return \Deli\Classes\NutrientAmountWithUnit::createFromStrings($match['nutrientName'], $match['amountWithUnit']);
 			}, $matches);
 		}
 
 		return null;
 	}
-
 }
--- src/Classes/Sources/itesco_cz/SourceProduct.php
+++ src/Classes/Sources/itesco_cz/SourceProduct.php
@@ -2,16 +2,18 @@
 
 namespace Deli\Classes\Sources\itesco_cz;
 
-class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
-
-	public function getURL() {
+class SourceProduct extends \Deli\Classes\Sources\SourceProduct
+{
+	public function getURL()
+	{
 		return 'https://nakup.itesco.cz/groceries/cs-CZ/products/' . $this->getProduct()->uri;
 	}
 
 	/****************************************************************************
 	 * Product details.
 	 */
-	public function loadDetails() {
+	public function loadDetails()
+	{
 		$product = $this->getProduct();
 
 		$product->setRemoteId($product->uri);
@@ -20,7 +22,7 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 		$product->update('name', $name);
 		$product->update('originalName', $name);
 
-		$remoteCategory = array_values(array_filter($this->getDOM()->filter('.breadcrumbs ol li')->each(function($e) {
+		$remoteCategory = array_values(array_filter($this->getDOM()->filter('.breadcrumbs ol li')->each(function ($e) {
 			return trim($e->text());
 		})));
 		$product->setRemoteCategory($remoteCategory);
@@ -35,8 +37,9 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Product information.
 	 */
-	public function loadProductInfos() {
-		return new \Deli\Classes\ProductInfos($this->getDOM()->filter('.brand-bank--brand-info .groupItem')->each(function($e) {
+	public function loadProductInfos()
+	{
+		return new \Deli\Classes\ProductInfos($this->getDOM()->filter('.brand-bank--brand-info .groupItem')->each(function ($e) {
 			return new \Deli\Classes\ProductInfo($e->filter('h3')->text(), preg_replace("/<h3>.+<\/h3>/", null, $e->html()));
 		}));
 	}
@@ -44,7 +47,8 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Allergens.
 	 */
-	public function loadAllergens() {
+	public function loadAllergens()
+	{
 		return \Deli\Models\ProductAllergen::getCodesFromStrings([
 			strip_tags($this->loadProductInfos()->filterByTitle('Složení')[0]->text),
 		]);
@@ -53,7 +57,8 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Product amount with unit.
 	 */
-	public function loadProductAmountWithUnit() {
+	public function loadProductAmountWithUnit()
+	{
 		try {
 			$dom = \Katu\Utils\DOM::crawlHtml($this->loadProductInfos()->filterByTitle('Výživové hodnoty')[0]->text);
 			return \Deli\Classes\AmountWithUnit::createFromString($dom->filter('table thead th')->eq(1)->html());
@@ -65,10 +70,11 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Nutrients.
 	 */
-	public function loadNutrients() {
+	public function loadNutrients()
+	{
 		try {
 			$dom = \Katu\Utils\DOM::crawlHtml($this->loadProductInfos()->filterByTitle('Výživové hodnoty')[0]->text);
-			return $dom->filter('tbody tr')->each(function($e) {
+			return $dom->filter('tbody tr')->each(function ($e) {
 				$nutrientName = $e->filter('td')->eq(0)->text();
 				$amountWithUnitString = explode('/', $e->filter('td')->eq(1)->text())[0];
 				return \Deli\Classes\NutrientAmountWithUnit::createFromStrings($nutrientName, $amountWithUnitString);
@@ -81,7 +87,8 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 	/****************************************************************************
 	 * Price.
 	 */
-	public function loadPrice() {
+	public function loadPrice()
+	{
 		//var_dump(\Deli\Classes\AmountWithUnit::createFromString($this->getDOM()->filter('.product-overview .price-per-quantity-weight')->text(), ['Kč']));
 
 		$pricePerProduct = \Deli\Classes\AmountWithUnit::createFromString($this->getDOM()->filter('.product-overview .price-per-sellable-unit')->text(), ['Kč']);
@@ -90,5 +97,4 @@ class SourceProduct extends \Deli\Classes\Sources\SourceProduct {
 
 		return $price;
 	}
-
 }
