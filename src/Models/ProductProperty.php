3157234cce3454cb569fbe55211ea15e9a4fff4d
Jan Jablunka <jablunka@gmail.com>
2020-02-17 17:01:13 +0100

    incremental

--- src/Models/ProductProperty.php
+++ src/Models/ProductProperty.php
@@ -2,22 +2,22 @@
 
 namespace Deli\Models;
 
-class ProductProperty extends \Deli\Model {
-
-	const TABLE = 'deli_product_properties';
-
+class ProductProperty extends \Deli\Model
+{
 	const SOURCE_ORIGIN = 'origin';
 	const SOURCE_VISCOKUPUJES_CZ = 'viscokupujes_cz';
+	const TABLE = 'deli_product_properties';
 
-	public function setValue($value) {
+	public function setValue($value)
+	{
 		$value = \Katu\Utils\JSON::encodeInline($value);
 		$this->update('value', in_array($value, ['null', '""', '[]']) ? null : $value);
 
 		return true;
 	}
 
-	public function getValue() {
+	public function getValue()
+	{
 		return \Katu\Utils\JSON::decodeAsArray($this->value);
 	}
-
 }
