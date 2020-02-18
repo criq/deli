09cba25737434cf2c7f6cf6dc8d28dbf9fa49313
Jan Jablunka <jablunka@gmail.com>
2020-02-18 11:14:46 +0100

    incremental

--- src/Models/RemoteCategory.php
+++ src/Models/RemoteCategory.php
@@ -5,4 +5,9 @@ namespace Deli\Models;
 class RemoteCategory extends \Deli\Model
 {
 	const TABLE = 'deli_remote_categories';
+
+	public function getArray()
+	{
+		return json_decode($this->json);
+	}
 }
