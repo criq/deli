90a1339c1a0f5f9aebf6de0287a984aaf3fe2c7b
Jan Jablunka <jablunka@gmail.com>
2020-02-19 13:58:34 +0100

    incremental

--- src/Classes/Sources/pbd_online_sk/ProductNutrient.php
+++ src/Classes/Sources/pbd_online_sk/ProductNutrient.php
@@ -2,8 +2,7 @@
 
 namespace Deli\Models\pbd_online_sk;
 
-class ProductNutrient extends \Deli\Models\ProductNutrient {
-
+class ProductNutrient extends \Deli\Models\ProductNutrient
+{
 	const TABLE = 'deli_pbd_online_sk_product_nutrients';
-
 }
